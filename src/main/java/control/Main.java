1d1b6ee553b914b59f383e4cff02bd98edc7b5c3
szllsi.marcell <szllsi.marcell@gmail.com>
2018-08-07 15:44:14 +0200

    gradle project added

---
+++ src/main/java/control/Main.java
@@ -0,0 +1,10 @@
+package control;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+    }
+}
